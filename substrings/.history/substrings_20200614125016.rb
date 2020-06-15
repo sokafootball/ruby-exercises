bafbb09119dbd083327abdcae3e14274935191d9
Laurent Capello <laurent.capello@gmail.com>
2020-06-15 19:32:20 +0200

    implemented substrings method

---
+++ substrings/.history/substrings_20200614125016.rb
@@ -0,0 +1,10 @@
+require 'pry'
+#definire un metodo che prende una stringa String e un array Substring di stringhe.
+#ritornare un hash le cui key sono stringhe dell'array contenuto nella stringa ed il valore il numero di volte che sono contenute
+
+#definisco un hash di ritorno substrings_hash
+#per ogni stringa in Substring
+  #se substring e' contenuta in String
+  #conto il numero di volte che e' contenuta in string
+  #aggiungo una key corrispondente in substring_hash e la valorizzo col numero trovato
+#ritorno substrings_hash
---
+++ substrings/.history/substrings_20200614125929.rb
@@ -0,0 +1,17 @@
+require 'pry'
+#definire un metodo che prende una stringa String e un array Substring di stringhe.
+#ritornare un hash le cui key sono stringhe dell'array contenuto nella stringa ed il valore il numero di volte che sono contenute
+def substrings(string, dictionary)
+  substrings_hash = {}
+  arr_of_strings = string.downcase.split
+end
+#definisco un hash di ritorno substrings_hash
+#definisco un array arr_of_strings che contiente String.downcase.split
+#per ogni stringa in Substring
+  #definisco variabile counter uguale a 0
+  #per ogni string in arr_of_strings
+   #se substring e' contenuta in string
+   #incremento counter
+  #se counter > 0
+    #aggiungo una key corrispondente in substring_hash e la valorizzo a counter
+#ritorno substrings_hash
