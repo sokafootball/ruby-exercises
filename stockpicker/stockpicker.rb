2675f2896145bf3a8b2f3acf36d3301c4005fc73
Laurent Capello <laurent.capello@gmail.com>
2020-06-16 19:38:11 +0200

    deleted comments in stockpicker, created bubble sort file

--- stockpicker/stockpicker.rb
+++ stockpicker/stockpicker.rb
@@ -25,14 +25,3 @@ def stock_picker(prices)
   return profits_per_day.max_by{|k,v| k}[1]
 end
 p stock_picker([17,3,6,9,15,8,6,1,10])
-#definisci hash PROFITS_PER_DAY
-#per ogni VALORE di prices
-  #se e' l'ultimo valore esci
-  #definisci variabile PROFIT uguale a nextValue - VALORE
-  #se INDEX di VALORE + 2 = length - 1
-    #salva in PROFITS_PER_DAY la key corrispondente a VALORE e value a PROFIT
-    #esci
-  #per ogni PRICE in prices a partire da index di VALORE + 2
-    #PROFIT = PRICE - VALUE > PROFIT ? PRICE - VALUE : PROFIT
-  #salva in PROFITS_PER_DAY la key corrispondente a VALORE e value a PROFIT
-#ritorna un array due elementi dove il primo e' la key con valore piu' alto ed il secondo il valore della key
