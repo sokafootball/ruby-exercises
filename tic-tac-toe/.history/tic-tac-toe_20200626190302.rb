eedd9ba0113ca3d7a49ae3f17dc0350ee604f749
Laurent Capello <laurent.capello@gmail.com>
2020-06-26 19:31:08 +0200

    added pseudocode file and main ruby file

---
+++ tic-tac-toe/.history/tic-tac-toe_20200626190302.rb
@@ -0,0 +1,23 @@
+#welcome the player to the game
+#ask to press a button to start a game
+#when he press the button, start the game
+  #change the status of the game
+  #start the game loop
+
+#let the player start
+
+#show the board
+#show legend of buttons assigned to each of the nine squares
+#if nobody won and there are empty cells
+
+#ask the player to choose an empty cell
+#if the cell is full, ask him again until he chooses an empty one
+#check if someone won
+#place the enemy token in a random empty cell
+#check if someone won
+
+
+#if someone won
+  #show a message depending on who won
+  #change the game status
+  #ask the player to press a button to start a new game
---
+++ tic-tac-toe/.history/tic-tac-toe_20200626190601.rb
@@ -0,0 +1,27 @@
+#welcome the player to the game
+#ask to press a button to start a game
+#when he press the button, start the game
+  #change the status of the game
+  #start the game loop
+
+#set random starting player
+
+#show the board
+#show legend of buttons assigned to each of the nine squares
+#if nobody won and there are empty cells
+  #run a turn
+
+
+#RUN A TURN
+#if it's player turn
+  #ask the player to choose an empty cell
+  #if the cell is full, ask him again until he chooses an empty one
+#else
+#place the enemy token in a random empty cell
+
+
+#
+#if someone won
+  #show a message depending on who won
+  #change the game status
+  #ask the player to press a button to start a new game
